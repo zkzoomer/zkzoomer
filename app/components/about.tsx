1ff2dbb9c0a7f3fe6b6c2322b13afdfddff097d4
zkzoomer <sergio@zkzoomer.io>
2024-09-08 19:36:20 +0800

    bug fixes

--- app/components/about.tsx
+++ app/components/about.tsx
@@ -4,12 +4,13 @@ import TextSection from '@/components/templates/text-section';
 
 const aboutText = (
   <div>
-    Versatile and self-taught blockchain engineer with a recent focus in ZK scaling solutions and a strong problem-solving background in aerospace.
-    I have led the design of cost-efficient protocols and custom ZK VMs, with an emphasis in upgradeability. 
+    Versatile and self-taught blockchain engineer with a recent focus in ZK scaling solutions and a
+    strong problem-solving background in aerospace. I have led the design of cost-efficient
+    protocols and custom ZK VMs, with an emphasis in upgradeability.
     <div className="h-2" />
     Freedom and Ethereum maximalist looking to push the limits of programmable money.
   </div>
-)
+);
 
 const About: React.FC = () => {
   return <TextSection symbol={<Info />} name="About" text={aboutText} />;
